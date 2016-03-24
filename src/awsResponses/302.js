51f78a08c72bfe503936168cfd1c738b075ade9d
Chris Patten <chris_patten@volusion.com>
2016-03-24 10:18:12 -0500

    Update the header.

--- src/awsResponses/302.js
+++ src/awsResponses/302.js
@@ -5,7 +5,8 @@ module.exports = {
   "responseParameters": {
     "method.response.header.x-vol-correlation": "integration.response.header.x-vol-correlation",
     "method.response.header.x-mat-correlation": "integration.response.header.x-mat-correlation",
-    "method.response.header.Access-Control-Allow-Origin": "'*'"
+    "method.response.header.Access-Control-Allow-Origin": "'*'",
+    "method.response.header.Location": "integration.response.header.Location",
   },
   "responseTemplates": {
     "application/json": null
