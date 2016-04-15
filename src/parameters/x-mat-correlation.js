59d2186e9c508d24dff35cd8690e7c87a73c6393
Richard Reedy <richard_reedy@volusion.com>
2016-04-15 12:38:33 -0500

    added x-mat-correlation to parameters

---
+++ src/parameters/x-mat-correlation.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  name: 'x-mat-correlation',
+  description: 'The correlation identifier of the call.',
+  in: 'header',
+  type: 'string',
+  required: false
+};
