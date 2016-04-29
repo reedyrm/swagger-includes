51d246bf33f7b24a8dd5bdd56f1bfc9805713341
Richard Reedy <richard_reedy@volusion.com>
2016-04-29 10:55:56 -0500

    added x-vol-correlation parameter

---
+++ src/parameters/x-vol-correlation.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  name: 'x-vol-correlation',
+  description: 'The correlation identifier of the call.',
+  in: 'header',
+  type: 'string',
+  required: false
+};
